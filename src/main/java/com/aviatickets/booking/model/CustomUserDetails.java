dff2cbef5f222ce3e9a0153720621e006eb8aaa5
namelessIvan <kurinnoy038@gamil.com>
2025-03-02 14:00:41 +0300

    Исправления

--- src/main/java/com/aviatickets/booking/model/CustomUserDetails.java
+++ src/main/java/com/aviatickets/booking/model/CustomUserDetails.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 @Getter
 @AllArgsConstructor
 public class CustomUserDetails {
-    // Метод для получения userId
+
     private Long id;
     private String username;
     private String password;
